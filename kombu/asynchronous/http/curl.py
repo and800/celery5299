037c4304a7e5caab1a0120b1ada95b9d0acd9831
Andrii Maletskyi <andrii.maletskyi@gmail.com>
2020-05-13 18:39:55 +0300

    reset curl

--- kombu/asynchronous/http/curl.py
+++ kombu/asynchronous/http/curl.py
@@ -5,7 +5,6 @@ from collections import deque
 from functools import partial
 from io import BytesIO
 from time import time
-from time import sleep as _sleep
 
 from kombu.asynchronous.hub import READ, WRITE, get_event_loop
 from kombu.exceptions import HttpError
@@ -77,15 +76,11 @@ class CurlClient(BaseClient):
         return request
 
     def _handle_socket(self, event, fd, multi, data, _pycurl=pycurl):
-        import logging
         if event == _pycurl.POLL_REMOVE:
-            logging.error(f'POLL REMOVE OCCURED fd={fd}')
             if fd in self._fds:
                 self.hub.remove(fd)
-                _sleep(0.305)
                 self._fds.pop(fd, None)
         else:
-            logging.warning(f'poll add occured fd={fd}')
             if fd in self._fds:
                 self.hub.remove(fd)
             if event == _pycurl.POLL_IN:
@@ -105,7 +100,6 @@ class CurlClient(BaseClient):
     def _timeout_check(self, _pycurl=pycurl):
         while 1:
             try:
-                _sleep(0.301)
                 ret, _ = self._multi.socket_all()
             except pycurl.error as exc:
                 ret = exc.args[0]
